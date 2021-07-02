b206872b1230f3cfe45ae75a59426be1e7acde52
bxz0206@163.com <bxz0206@163.com>
2021-07-02 17:52:06 +0800

    set与Map

--- 常用代码/ES6/set和Map.js
+++ 常用代码/ES6/set和Map.js
@@ -1,9 +1,9 @@
 /*
  * @Author: your name
  * @Date: 2021-06-30 17:09:44
- * @LastEditTime: 2021-07-01 16:23:13
+ * @LastEditTime: 2021-07-02 13:55:01
  * @LastEditors: Please set LastEditors
- * @Description: es6 Set  Map
+ * @Description: es6 Set
  * @FilePath: \常用代码\ES6\set和Map.js
  */
 'Set是一个构造函数，用来生成Set数据结构;类似数组,但成员的值是唯一的,没有重复值'
@@ -61,6 +61,38 @@ for(let i of a.entries()){
 '扩展运算符和Set结构结合，可以去除数组的重复成员'
 let set = new Set(['red', 'green', 'blue'])
 let arr = [...set]
+let arr1 = [...new Set([5,1,4,2,3,6,1,,5,2,48,2,7])]
+'map和fliter方法也可以间接用于Set'
+let set1 = new Set([1, 2, 3, 4, 5])
+let map1 = new Set([...set1].map(i => i +1))
+let filter1 = new Set([...set1].filter(i => i >3))
+
+'set可以实现 交集 并集 差集'
+// let b = [2,4,5,3,6,8,4,5,1,7,8,2,0]
+'并集'
+let union = new Set([...set1,...b])
+console.log(union)
+'交集'
+let intersect  = new Set([...b].filter(i => set1.has(i)))
+console.log(intersect)
+'差集'
+let difference = new Set([...b].filter(i => !set1.has(i)))
+console.log(difference)
+
+/*
+ * @Author: your name
+ * @Date: 2021-06-30 17:09:44
+ * @LastEditTime: 2021-07-02 10:02:47
+ * @LastEditors: Please set LastEditors
+ * @Description: es6 weakSet
+ * @FilePath: \常用代码\ES6\set和Map.js
+ */
+
+'WeakSet 不重复的值的集合，其结构与Set类似'
+'WeakSet与Set区别:(1)WeakSet成员只能是对象;'
+'(2)WeakSet对象是弱引用,即垃圾机制不考虑WeakSet对该对象的使用,因此WeakSet不适于引用。ES6规定WeakSet不可遍历'
+'WeakSet 三个方法：(1)add: 向WeakSet添加新元素(2)delete：清除WeakSet实例的指定成员(3)has：判断某个值是否在WeakSet实例之中'
+'WeakSet 没有size属性，无法遍历其成员。用处：存储dom节点'
 ''
 ''
 ''
